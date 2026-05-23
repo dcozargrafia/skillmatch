e7fa4cfe98d86eef8286c61e9573c9bc138e4a0c
Daniel Cózar <dcozargrafia@gmail.com>
2026-05-23 13:53:00 +0200

    fix: crear proyecto ONG usa validateProject en vez de validateNgoProfile
    - Agrega validateProject en src/domain/project/Project.js con validación de título obligatorio y mensaje en español
    - Reemplaza import y llamada a validateNgoProfile por validateProject en createProjectUseCase
    - Actualiza mocks y aserciones en test del use case
    - 5 nuevos tests de dominio, 684 tests pasando

--- src/application/project/createProjectUseCase.js
+++ src/application/project/createProjectUseCase.js
@@ -1,10 +1,10 @@
 /**
  * Caso de uso: crear un proyecto.
- * Valida el perfil NGO y luego llama a la API.
+ * Valida el proyecto y luego llama a la API.
  */
 
 import { createProject } from '../../infrastructure/api/projectApi.js';
-import { validateNgoProfile } from '../../domain/ngo/Ngo.js';
+import { validateProject } from '../../domain/project/Project.js';
 
 /**
  * @param {object} projectData
@@ -12,7 +12,7 @@ import { validateNgoProfile } from '../../domain/ngo/Ngo.js';
  * @throws {Error} Si la validación falla o la API falla
  */
 export async function createProjectUseCase(projectData) {
-  const { errors } = validateNgoProfile(projectData);
+  const { errors } = validateProject(projectData);
   if (Object.keys(errors).length > 0) {
     const firstError = Object.values(errors)[0];
     throw new Error(firstError);
