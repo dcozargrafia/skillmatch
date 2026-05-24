73af5c4bbfbc41031b71cd35c381f5bf6147fbbf
Daniel Cózar <dcozargrafia@gmail.com>
2026-05-24 19:57:36 +0200

    actualizacion resto de paginas

--- src/ui/layouts/AdminLayout.jsx
+++ src/ui/layouts/AdminLayout.jsx
@@ -9,7 +9,7 @@ function AdminLayout() {
     <>
       <header className="app-header">
         <Link to="/admin" className="app-header__brand">
-            <img src="/branding/logo.svg" alt="SkillMatch" className="app-header__logo" />
+            <img src="/branding/logo-navbar-tight.svg" alt="SkillMatch" className="app-header__logo" />
         </Link>
         <div className="app-header__user">
           <span className="app-header__name">{user?.name} · Admin</span>
